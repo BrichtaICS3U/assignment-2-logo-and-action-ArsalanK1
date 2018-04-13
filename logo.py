ae395e58ba034605e024a0956fde5c8a5327fbab
ARSALAN KHAN <S338968746@ocdsb>
2018-04-13 09:42:31 -0400

    finished teh SHIELD logo

--- logo.py
+++ logo.py
@@ -5,6 +5,7 @@
 # adapted from http://www.101computing.net/getting-started-with-pygame/
 
 # Import the pygame library and initialise the game engine
+import math
 import pygame
 pygame.init()
 
@@ -14,6 +15,8 @@ BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
+BLUE = (77, 121, 255)
+YELLOW = (255, 214, 51)
 
 # Set the screen size (please don't change this)
 SCREENWIDTH = 400
@@ -23,7 +26,7 @@ SCREENHEIGHT = 400
 # The window is defined as (width, height), measured in pixels
 size = (SCREENWIDTH, SCREENHEIGHT)
 screen = pygame.display.set_mode(size)
-pygame.display.set_caption("My Logo")
+pygame.display.set_caption("SHIELD Logo")
 
 # This loop will continue until the user exits the game
 carryOn = True
@@ -48,8 +51,34 @@ while carryOn:
 
     # Queue different shapes and lines to be drawn
     # pygame.draw.rect(screen, RED, [55, 200, 100, 70], 0)
-    # pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
-    # pygame.draw.ellipse(screen, BLACK, [20, 20, 250, 100], 2)
+    #GRID
+    pygame.draw.line(screen, BLACK, [0, 100], [400, 100], 1)
+    pygame.draw.line(screen, BLACK, [0, 200], [400, 200], 1)
+    pygame.draw.line(screen, BLACK, [0, 300], [400, 300], 1)
+    pygame.draw.line(screen, BLACK, [100, 0], [100, 400], 1)
+    pygame.draw.line(screen, BLACK, [200, 0], [200, 400], 1)
+    pygame.draw.line(screen, BLACK, [300, 0], [300, 400], 1)
+    #CIRCLES
+    pygame.draw.ellipse(screen, BLACK, [50,50, 300, 300], 0)
+    pygame.draw.ellipse(screen, WHITE, [63,63, 275, 275], 0)
+    #MIDDLE SEP
+    pygame.draw.line(screen, BLACK, [75, 125], [200, 200], 16)
+    pygame.draw.line(screen, BLACK, [325, 125], [200, 200], 16)
+    #WINGS LEFT
+    pygame.draw.line(screen, BLACK, [107, 143], [60, 237], 13)
+    pygame.draw.line(screen, BLACK, [140, 160], [80, 283], 13)
+    pygame.draw.line(screen, BLACK, [175, 185], [110, 313], 13)
+    #WINGS RIGHT
+    pygame.draw.line(screen, BLACK, [293, 143], [340, 235], 13)
+    pygame.draw.line(screen, BLACK, [260, 160], [320, 280], 13)
+    pygame.draw.line(screen, BLACK, [225, 185], [290, 313], 12)
+    #HEAD LEFT
+    pygame.draw.rect(screen, BLACK, [145, 60, 110, 13], 0)
+    pygame.draw.polygon(screen, BLACK, [[110, 85], [170, 120], [190, 55]], 0)
+    #HEAD RIGHT
+    pygame.draw.polygon(screen, BLACK, [[290, 85], [230, 120], [210, 55]], 0)
+    pygame.draw.polygon(screen, WHITE, [[210, 73], [230, 73], [235, 83], [210, 83]], 0)
+    
 
     # Update the screen with queued shapes
     pygame.display.flip()
